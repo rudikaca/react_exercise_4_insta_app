dd295e1a4d47a363b5196c50661ab4f2d76ebce5
rudinkaca <rudin.kaca@crispybacon.al>
2022-11-17 12:29:01 +0100

    feat: show posts with mock data and styled post component

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,11 +1,29 @@
 import React from "react";
 import CommentSection from "../CommentSection/CommentSection";
+import './PostContainer.css';
+import {FaRegComment, FaRegHeart} from "react-icons/fa";
 
-const PostContainer = () => {
+const PostContainer = ({data}) => {
     return (
-        <div>
-            <CommentSection />
-        </div>
+        <>
+            {data.map((post) => <div className="post-container">
+                <div>
+                    <div className="title">
+                        <img className="profile-image" src={post.thumbnailUrl} alt="logo"/>
+                        <p><strong>{post.username}</strong></p>
+                    </div>
+                    <div>
+                        <img className="image" src={post.imageUrl} alt=""/>
+                    </div>
+                    <div className="icon-container">
+                        <FaRegHeart /><FaRegComment />
+                    </div>
+                    <p className="likes"><strong>{post.likes} likes</strong></p>
+
+                </div>
+                <CommentSection data={data} />
+            </div>)}
+        </>
     )
 }
 
